a056de6a9fa435e38c455201835bab8a91865fe8
Prashant <fslabsios@gmail.com>
2015-07-30 12:59:17 +0530

    Initial Commit

---
+++ Memorization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memorization
+//
+//  Created by itrentalindia on 7/30/15.
+//  Copyright (c) 2015 fourthscreenlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MemorizationTests/MemorizationTests.m
@@ -0,0 +1,40 @@
+//
+//  MemorizationTests.m
+//  MemorizationTests
+//
+//  Created by itrentalindia on 7/30/15.
+//  Copyright (c) 2015 fourthscreenlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MemorizationTests : XCTestCase
+
+@end
+
+@implementation MemorizationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
